ede5592d13ff68eeb95981059b3e69782a76ac3e
Felix Schmitz <felixschmitz57@gmail.com>
2024-10-27 16:42:52 +0100

    Writing initial missing data regression task.

--- src/missing_data_gmm/missreg/task.py
+++ src/missing_data_gmm/missreg/task.py
@@ -1,2 +1,8 @@
-def task_do_something() -> None:
-    return None
+from pathlib import Path
+from typing import Annotated
+
+from missing_data_gmm.config import DATA, pd
+
+
+def task_do_something(raw_data: Annotated[Path, DATA / "wls-data.dta"]) -> None:
+    df = pd.read_stata(DATA / "wls-data.dta")
