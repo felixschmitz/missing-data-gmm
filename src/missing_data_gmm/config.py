b9d52fb68f72aae7b6d2686bf9e8b66fa18ddf3d
Felix Schmitz <felixschmitz57@gmail.com>
2024-11-26 16:33:13 +0100

    Update gitignore and introduce DataCatalog structure.

--- src/missing_data_gmm/config.py
+++ src/missing_data_gmm/config.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import pandas as pd
+from pytask import DataCatalog
 
 pd.set_option("mode.copy_on_write", True)
 pd.set_option("future.infer_string", True)
@@ -14,9 +15,27 @@ DATA = SRC.joinpath("..", "..", "data").resolve()
 
 TEST_DIR = SRC.joinpath("..", "..", "tests").resolve()
 
+SEX_NAMES = ("male", "not-male")
+DATA_NAMES = ("wls-data", "wls-data-adultbmi")
+
+DATA_CATALOGS = {
+    "regression": {
+        data_name: {
+            sex_name: DataCatalog(name=f"{data_name}-{sex_name}")
+            for sex_name in SEX_NAMES
+        }
+        for data_name in DATA_NAMES
+    },
+    "simulation": DataCatalog(name="MC"),
+}
+
 
 __all__ = [
+    "pd",
     "DATA",
     "SRC",
     "TEST_DIR",
+    "SEX_NAMES",
+    "DATA_NAMES",
+    "DATA_CATALOGS",
 ]
